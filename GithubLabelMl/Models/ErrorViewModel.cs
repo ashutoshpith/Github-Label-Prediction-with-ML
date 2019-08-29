a8ef020622594eb598ea1c66de84af4e058ab194
ashutoshpith <amxdp18@gmail.com>
2019-08-29 21:20:54 +0530

    dotnet core mvc created

---
+++ GithubLabelMl/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GithubLabelMl.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
