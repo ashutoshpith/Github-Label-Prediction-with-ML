cce2a8b331952abb4db6bb70837687f9f15a83e6
ashutoshpith <amxdp18@gmail.com>
2019-08-29 22:00:32 +0530

    model train added

---
+++ GithubLabelMl/Models/Gitpred.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.ML;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GithubLabelMl.Models
+{
+    public class Gitpred
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public string Area { get; set; }
+
+     
+    }
+}
---
+++ GithubLabelMlML.Model/DataModels/ModelInput.cs
@@ -0,0 +1,30 @@
+//*****************************************************************************************
+//*                                                                                       *
+//* This is an auto-generated file by Microsoft ML.NET CLI (Command-Line Interface) tool. *
+//*                                                                                       *
+//*****************************************************************************************
+
+using Microsoft.ML.Data;
+
+namespace GithubLabelMlML.Model.DataModels
+{
+    public class ModelInput
+    {
+        [ColumnName("ID"), LoadColumn(0)]
+        public float ID { get; set; }
+
+
+        [ColumnName("Area"), LoadColumn(1)]
+        public string Area { get; set; }
+
+
+        [ColumnName("Title"), LoadColumn(2)]
+        public string Title { get; set; }
+
+
+        [ColumnName("Description"), LoadColumn(3)]
+        public string Description { get; set; }
+
+
+    }
+}
